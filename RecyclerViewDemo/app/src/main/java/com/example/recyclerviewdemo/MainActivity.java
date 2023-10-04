94a9e9ba2c9cdefa4aad09efab1f6410cd0c8d58
Kediyal <kediyalr@student.douglascollege.ca>
2023-10-04 12:19:18 -0700

    Getting Error need to fix it

--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.recyclerviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.example.recyclerviewdemo.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import adapter.TuneAdapter;
+import model.Tune;
 
 public class MainActivity extends AppCompatActivity {
     List<String> TuneNames = new ArrayList<>(Arrays.asList("Beauty and the Beast",
@@ -14,10 +21,31 @@ public class MainActivity extends AppCompatActivity {
     List<Integer> TunePics = new ArrayList<>(Arrays.asList(R.drawable.beauty,
             R.drawable.lionking, R.drawable.marypoppins,
             R.drawable.gameofthrones, R.drawable.ozark ));
+    List<Tune> TuneList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+        LoadModel();
+        TuneAdapter tuneAdapter = new TuneAdapter(TuneList);
+        LinearLayoutManager lm = new LinearLayoutManager(this);
+        binding.reccyclerViewTunes.setAdapter(tuneAdapter);
+        binding.reccyclerViewTunes.setLayoutManager(lm);
+
+
     }
+
+    private void LoadModel()
+    {
+        for(int i=0;i<TuneNames.size();i++)
+        {
+            Tune eachTune  = new Tune(TuneNames.get(i),TunePics.get(i));
+            TuneList.add(eachTune);
+        }
+    }
+
 }
