58ab18bebdc7d8628872a8d9c4959f6b9bd55b2d
Kediyal <kediyalr@student.douglascollege.ca>
2023-12-12 12:45:27 -0800

    Started doing the practice

---
+++ FinalPractice/app/src/main/java/com/example/finalpractice/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.finalpractice;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.finalpractice.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Cool", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // make a list of Phone Type
+        List<Phone> phoneList = new ArrayList<>();
+
+        // Adding manual Data into the List
+        List<String> phoneDesc = new ArrayList<>(Arrays.asList("Nakia","Blackbeery","Lenova","Asus"));
+        List<Integer> phoneColor = new ArrayList<>(Arrays.asList(R.color.))
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(this,"Clicked on settings",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if(id == R.id.action_search) {
+            Toast.makeText(this,"Clicked on Search",Toast.LENGTH_SHORT).show();
+            return true;
+        } else if(id == R.id.action_profile) {
+            Toast.makeText(this,"Clicked on userprofile",Toast.LENGTH_SHORT).show();
+             return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
---
+++ FinalPractice/app/src/main/java/com/example/finalpractice/Phone.java
@@ -0,0 +1,26 @@
+package com.example.finalpractice;
+
+public class Phone {
+    String phoneColor;
+    String phoneName;
+
+    public Phone(String phoneColor) {
+        this.phoneColor = phoneColor;
+    }
+
+    public String getPhoneColor() {
+        return phoneColor;
+    }
+
+    public void setPhoneColor(String phoneColor) {
+        this.phoneColor = phoneColor;
+    }
+
+    public String getPhoneName() {
+        return phoneName;
+    }
+
+    public void setPhoneName(String phoneName) {
+        this.phoneName = phoneName;
+    }
+}
